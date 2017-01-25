7a4e8382f0d3ec04590a6fe8acfa62a21ef34043
Buisson <colombet.aurelien@etu.unice.fr>
2017-01-25 23:10:05 +0100

    Proxy Bean to do get and post request

---
+++ Proxy/Proxy/Proxy.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * Crée par SharpDevelop.
+ * Utilisateur: Meulon
+ * Date: 25/01/2017
+ * Heure: 19:04
+ * 
+ * Pour changer ce modèle utiliser Outils | Options | Codage | Editer les en-têtes standards.
+ */
+using System;
+using WComp.Beans;
+using Newtonsoft.Json;
+
+namespace WComp.Beans
+{
+
+	[Bean(Category="Flhubber")]
+	public class Proxy
+	{
+
+		private string port = "8181"; 
+		private string ip = "10.0.0.3";
+		//private string getVal = "";
+		
+		public string Port {
+			get { return port; }
+			set {
+				port = value;
+			}
+		}
+		
+		public string IP {
+			get { return ip; }
+			set {
+				ip = value;
+			}
+		}
+		
+		public string get(string url){
+			string tmp = Url.GET(url);
+			getVal(tmp);
+			return tmp;
+		}
+		
+		public delegate string getBodyOfGETHandler(string url);
+		
+		public event getBodyOfGETHandler getVal;
+		
+		private string getBodyOfGET(string url){
+			string tmp = Url.GET(url);
+			getVal(tmp);
+			return tmp;
+		}
+		
+		
+
+
+	}
+}
