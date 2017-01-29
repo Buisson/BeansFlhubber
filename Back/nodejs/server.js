202153b16bff45e841c3e84efe1edd2ea439d4ce
Persenda Edgar <edgarpersenda@wanadoo.fr>
2017-01-29 16:08:31 +0100

    kiiijupuiouoyesr_-dtèyuhkftuxcy

---
+++ Back/nodejs/server.js
@@ -0,0 +1,146 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+var bodyParser = require("body-parser");
+var mailer = require("nodemailer");
+
+app.use(express.static('public'));
+
+
+var smtpTransport = mailer.createTransport("SMTP",{
+					service: "Gmail",
+					auth: {
+						user: "flhubberocs@gmail.com",
+						pass: "polytech"
+					}
+				});
+
+
+var test = {
+	data: "test ok"
+};
+
+app.get('/test', function (req, res) {
+    console.log( "test" );
+    res.json( test );
+    res.end();
+});
+
+app.get('/json', function (req, res){
+	 var content = fs.readFileSync("config.json");
+	 var jsonContent = JSON.parse(content);
+	 res.send(jsonContent);
+	 res.end();
+});
+
+app.get('/sendMail', function (req, res){
+	 var content = fs.readFileSync("config.json");
+	 var jsonContent = JSON.parse(content);
+	 var id = req.query.id;
+	 if(jsonContent[id]) {
+		 var adress = jsonContent[id]["email"];
+
+		 if(adress == "None") {
+		 	res.send("Email not set");
+		 	res.end();
+		 	console.log("Email not set");
+		 	return;
+		 }
+
+		var mail = {
+			from: "flhubberocs@gmail.com",
+			to: adress,
+			subject: "Branche ton portable branleur",
+			html: "Qu'est ce que tu fous encore là, va brancher ton portable"
+		}
+
+		smtpTransport.sendMail(mail, function(error, response){
+						if(error){
+							console.log("Erreur lors de l'envoie du mail!");
+							console.log(error);
+						}else{
+							console.log("Mail envoyé avec succès!")
+						}
+						smtpTransport.close();
+					});
+	}
+	console.log("ID DONT EXIST");
+	res.end();
+});
+
+
+app.get('/getUsers', function (req,res)
+{
+	var content = fs.readFileSync("config.json");
+	var jsonContent = JSON.parse(content);
+	var users = Object.keys(jsonContent);
+	console.log(users);
+
+	res.send(users);
+	res.end();
+});
+
+app.get('/user', function (req, res){
+	 var content = fs.readFileSync("config.json");
+	 var jsonContent = JSON.parse(content);
+	 var id = req.query.id;
+	 var user = jsonContent[id];
+	 console.log("user", user);
+	 res.send(user);
+	 res.end();
+});
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.post('/form', function (req, res) {
+    console.log( "form", req.body );
+    var content = fs.readFileSync("config.json");
+	var jsonContent = JSON.parse(content);
+
+	jsonContent[req.body.id].email = req.body.email
+	
+	fs.writeFile('config.json', JSON.stringify(jsonContent), function (err) {
+	  if (err) return console.log(err);
+	  console.log('Json updated');
+	});	//todo save the json
+
+});
+
+
+app.get('/create', function (req, res) {
+
+	console.log('create', req.body);
+    var content = fs.readFileSync("config.json");
+	var jsonContent = JSON.parse(content);
+	var usersReceive = req.query.id.split(",");
+
+    var users = Object.keys(jsonContent);
+
+    for (var i = 0, len = users.length; i < len; i++) {
+    	for (var j = 0, len = usersReceive.length; j < len; j++) {
+	    	if(users[i] != usersReceive[j]) {
+    		    jsonContent[usersReceive[j]] = {"timeBeforeAlert":0,"email":"None"};
+	    	}
+		}
+	}
+
+    fs.writeFile('config.json', JSON.stringify(jsonContent), function (err) {
+	if (err) return console.log(err);
+	console.log('Json updated');
+	});
+});
+
+
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("Example app listening on port : %s", port);
+
+});
